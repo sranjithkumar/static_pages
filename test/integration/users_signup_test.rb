741d6a1e96dc9c8d9572542b88a3171e0d516017
sranjithkumar <ranjith@juhomi.com>
2016-03-24 17:22:18 +0530

    Ex:7.7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
@@ -22,5 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert-success'
   end
 end
